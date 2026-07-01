f35a18bc9a848297139c4083b474468b2f0c7c6a
yash kumar yadav <yyaduvanshi230@gmail.com>
2026-07-01 11:43:48 +0530

    Stage 2: Board display working

--- TIC-TAC-TOE.c
+++ TIC-TAC-TOE.c
@@ -1,7 +1,30 @@
 #include<stdio.h>
 #include<conio.h>
+#include<stdlib.h>
 int main()
 {
-printf("This is Yash going to build a TIC-TAC-TOE Game \n wish me luck ");
-return 0;
+ int arr[9],k;
+ for(k=0;k<9;k++){
+     arr[k]=k+1;
+ }
+ int i,j;
+ for(i=0;i<3;i++){
+    for(j=0;j<3;j++){
+        int index=i*3+j;
+         printf(" %d ",arr[index]);
+         if(j!=2){
+         printf("|");
+         }
+          
+         }
+         printf("\n");
+          if(i!=2){
+        printf("-----------");
+     printf("\n");
+          }
+    
+     
+    }
+   
+ return 0;
 }
